8d2ce1377757ca0e2126afd4388514a03826d8af
Andreas Zeitler <me@azeitler.com>
2025-02-26 23:02:39 +0100

    update fields for inbound_webhook

--- app/avo/resources/inbound_webhook.rb
+++ app/avo/resources/inbound_webhook.rb
@@ -8,6 +8,18 @@ class Avo::Resources::InboundWebhook < Avo::BaseResource
   def fields
     field :id, as: :id
     field :status, as: :select, enum: ::InboundWebhook.statuses
-    field :body, as: :textarea
+
+    field :body, as: :code, theme: 'dracula', language: 'json' do |model, resource, view|
+      JSON.pretty_generate(model.body)
+    end
+
+    field :controller_name, as: :text
+    field :action_name, as: :text
+    field :ip_address, as: :text
+    field :user_agent, as: :text
+
+    field :headers, as: :code, theme: 'dracula', language: 'json' do |model, resource, view|
+      JSON.pretty_generate(model.headers)
+    end
   end
 end
