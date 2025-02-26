9119ba3f3d886bd90f090c6f57c715a917979b4b
Andreas Zeitler <me@azeitler.com>
2025-02-26 23:27:58 +0100

    fix: webhook ui

--- app/avo/resources/inbound_webhook.rb
+++ app/avo/resources/inbound_webhook.rb
@@ -9,8 +9,8 @@ class Avo::Resources::InboundWebhook < Avo::BaseResource
     field :id, as: :id
     field :status, as: :select, enum: ::InboundWebhook.statuses
 
-    field :body, as: :code, theme: 'dracula', language: 'json' do |model, resource, view|
-      JSON.pretty_generate(resource.body)
+    field :body, as: :code, theme: 'dracula', language: 'json', format_using: -> (record) do
+      JSON.pretty_generate(record.body)
     end
 
     field :controller_name, as: :text
@@ -18,8 +18,8 @@ class Avo::Resources::InboundWebhook < Avo::BaseResource
     field :ip_address, as: :text
     field :user_agent, as: :text
 
-    field :headers, as: :code, theme: 'dracula', language: 'json' do |model, resource, view|
-      JSON.pretty_generate(resource.headers)
+    field :headers, as: :code, theme: 'dracula', language: 'json', format_using: -> (record) do
+      JSON.pretty_generate(record.headers)
     end
   end
 end
