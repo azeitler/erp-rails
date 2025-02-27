544a84165aa37548d57a5928b12784067b5ed200
Andreas Zeitler <me@azeitler.com>
2025-02-27 14:11:56 +0100

    refactor pipedrive job

--- app/jobs/inbound_webhooks/pipedrive_job.rb
+++ app/jobs/inbound_webhooks/pipedrive_job.rb
@@ -11,33 +11,25 @@ module InboundWebhooks
       @meta = inbound_webhook.params['meta']
       @action = meta['action']
       @entity = meta['entity']
+      @id = meta['entity_id']
       @event = "#{entity}.#{action}"
-
-      begin
-        ProcessPipedriveWebhookCommand.execute_for(inbound_webhook)
-      rescue StandardError => e
-        Rails.logger.error "#{self.class.name} for processing webhook failed: #{e}"
-        Bugsnag.add_metadata("Webhook Data", inbound_webhook.params)
-        Bugsnag.notify(e)
-      end
 
       Rails.configuration.event_store.publish(
         PipedriveEvent.new(data: {
           event: event,
+          id: @id
         }),
         stream_name: 'pipedrive'
       )
+      ProcessPipedriveWebhookCommand.execute_for(inbound_webhook)
 
       inbound_webhook.processed!
-
-      # Or mark as failed and re-enqueue the job
-      # inbound_webhook.failed!
+    rescue StandardError => e
+      Rails.logger.error "#{self.class.name} for processing webhook failed: #{e}"
+      Bugsnag.add_metadata("Webhook Data", inbound_webhook.params)
+      Bugsnag.notify(e)
+      inbound_webhook.failed!
     end
-
-    private
-      def perform_for_person
-
-      end
 
   end
 end
