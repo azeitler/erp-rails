da9e502731052399afda6d01115415ba4da42bbc
Andreas Zeitler <me@azeitler.com>
2025-02-27 17:50:58 +0100

    fix: linkedin invites migration

--- db/migrate/20250227164510_add_identifier_to_linkedin_invitation.rb
+++ db/migrate/20250227164510_add_identifier_to_linkedin_invitation.rb
@@ -1,5 +1,5 @@
 class AddIdentifierToLinkedinInvitation < ActiveRecord::Migration[7.2]
   def change
-    add_column :linkedin_invitations, :identifier, :string
+    add_column :linkedin_invites, :identifier, :string
   end
 end
