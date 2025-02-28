6040ec23bff047966b84d03290d83a96eadbfbdf
Andreas Zeitler <me@azeitler.com>
2025-02-28 14:45:24 +0100

    add: normalize properties to breakcold lead

--- app/models/breakcold/lead.rb
+++ app/models/breakcold/lead.rb
@@ -62,6 +62,7 @@ class Breakcold::Lead < ApplicationRecord
   end
 
   def parse
+    self.properties = normalize_properties
     self.email = properties['email']
 
     self.deleted = properties['is_deleted']
@@ -74,5 +75,36 @@ class Breakcold::Lead < ApplicationRecord
     end
 
     self.lists = Breakcold::List.where(identifier: list_ids)
+    true
+  end
+
+  def normalize_properties
+    normalize_hash(self.properties)
+  end
+  def normalize_hash(properties)
+    # "lists": [
+    #   {
+    #     "table": {
+    #       "id": "db37974f-8e20-4161-a09f-5a57363ee66d",
+    #       "name": "Sales App (Vuframe) >200 MA",
+    #       "emoji": "office",
+    #       "order": 9
+    #     }
+    #   }
+    # ],
+    # the properties hash contains a number of arrays which in turn contain arrays of hashes which might only contain a single key 'table', in this case we want to remove the intermediate hash
+    # we want to check any array keys for a hash with a single key 'table' and replace the array with the value of the 'table' key, even nested ones
+    properties.each do |key, value|
+      if value.is_a?(Array)
+        properties[key] = value.map do |item|
+          if item.is_a?(Hash) && item.keys.length == 1 && item['table']
+            normalize_hash(item['table'])
+          else
+            normalize_hash(item)
+          end
+        end
+      end
+    end
+    properties
   end
 end
