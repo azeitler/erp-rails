07193016451e9644ba997809953a6b71b0744aa8
Andreas Zeitler <me@azeitler.com>
2025-03-02 04:01:27 +0100

    add: event_label to LeadAddedToLemlistCampaignEvent

--- app/events/lead_added_to_lemlist_campaign_event.rb
+++ app/events/lead_added_to_lemlist_campaign_event.rb
@@ -6,4 +6,12 @@
 # -----
 
 class LeadAddedToLemlistCampaignEvent < ApplicationEvent
+  data_attribute :lead
+  data_attribute :breakcold_list
+  data_attribute :lemlist_campaign
+  data_attribute :status
+
+  def event_label
+    "#{lead} #{breakcold_list}[Breakcold] #{lemlist_campaign}[Lemlist] (#{super})"
+  end
 end
