e4c243311cc2fcdfa18f18ec375464d0f654cc7d
Andreas Zeitler <me@azeitler.com>
2025-03-02 03:26:23 +0100

    add: default to properties

--- db/migrate/20250301234836_add_fields_to_linkedin_invite.rb
+++ db/migrate/20250301234836_add_fields_to_linkedin_invite.rb
@@ -1,6 +1,6 @@
 class AddFieldsToLinkedinInvite < ActiveRecord::Migration[7.2]
   def change
-    add_column :linkedin_invites, :properties, :jsonb
+    add_column :linkedin_invites, :properties, :jsonb, default: {}
     add_column :linkedin_invites, :status_text, :string
     remove_column :linkedin_invites, :identifier
   end
